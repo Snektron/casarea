7c6e97378fd733cb54ad896a8fd5395f6647fed7
DDPS course <ddps2017@fs1.cm.cluster>
2020-11-02 22:04:59 +0100

    download_dataset.sh

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-BASE_URL="http://law.di.unimi.it/webdata"
+BASE_URL="http://data.law.di.unimi.it/webdata"
 DATASETS="uk-2007-05 twitter-2010"
+OUT_DIR="/var/scratch/ddps2017/datasets"
 
-for dataset in $DATSETS; do
+for dataset in $DATASETS; do
 	for ext in .graph .properties; do
-		wget $BASE_URL/$dataset$ext -P datasets/
+		file=$dataset$ext
+		if [ ! -f $OUT_DIR/$file ]; then
+			echo "Downloading $file"
+			wget -q -O $OUT_DIR/$file $BASE_URL/$dataset/$file
+		else
+			echo "Skipping $file (already present)"
+		fi
 	done
 done
