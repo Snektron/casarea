e672ff367aaa4091e2ae1340353dc16d919e8225
Robin Voetter <robin@voetter.nl>
2020-11-03 02:10:07 +0100

    Script setup

--- autorun.sh
+++ autorun.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # Check arguments
 if [ -z "$1" ]; then
---
+++ download-datasets.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# This file downloads required datasets
+# It should be called from casarea.sh
+
+BASE_URL="http://data.law.di.unimi.it/webdata"
+mkdir -p "$CASAREA_DATA/datasets/"
+
+for DATASET in $CASAREA_TEST_GRAPHS; do
+    for EXT in .graph .properties; do
+        FILE="$DATASET$EXT"
+        OUT="$CASAREA_DATA/datasets/$FILE"
+        wget -nc -q --show-progress -O $OUT "$BASE_URL/$DATASET/$FILE"
+    done
+done
