a2e562d4ecf961fd6c57ef8a72edee7c8b49ca84
Robin Voetter <robin@voetter.nl>
2020-11-03 15:33:06 +0100

    webgraph extractor

--- webgraph-extract/src/main/java/space/pythons/webgraph_extract/Main.java
+++ webgraph-extract/src/main/java/space/pythons/webgraph_extract/Main.java
@@ -1,17 +1,47 @@
 package space.pythons.webgraph_extract;
 
 import it.unimi.dsi.webgraph.BVGraph;
+import it.unimi.dsi.webgraph.NodeIterator;
+
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
 
 public class Main {
+    private static void writeIntLittleEndian(OutputStream out, int value) throws Throwable {
+        out.write(value & 0xFF);
+        out.write((value >> 8) & 0xFF);
+        out.write((value >> 16) & 0xFF);
+        out.write((value >> 24) & 0xFF);
+    }
+
+    private static void writeNodeEdges(BVGraph graph, NodeIterator it, OutputStream out) throws Throwable {
+        int src = it.nextInt();
+        int degree = it.outdegree();
+        int[] succ = it.successorArray();
+        for (int i = 0; i < degree; ++i) {
+            int dst = succ[i];
+            writeIntLittleEndian(out, src);
+            writeIntLittleEndian(out, dst);
+        }
+    }
+
     public static void main(String[] args) throws Throwable {
-        if (args.length != 1) {
-            System.err.println("Missing argument: <file.graph>");
+        if (args.length != 2) {
+            System.err.println("Missing argument: <graph basename> <edges output>");
             return;
         }
 
-        var source = args[0];
-        var graph = BVGraph.load(source, 0);
+        var graph = BVGraph.load(args[0], 0);
+        System.out.printf("Writing %.2f GB of edges", graph.numArcs() * 4 * 2 / 1e9);
+        try (var fos = new FileOutputStream(args[1])) {
+            var bos = new BufferedOutputStream(fos);
+            int total_nodes = graph.numNodes();
 
-        System.out.println(graph.numArcs());
+            var it = graph.nodeIterator();
+            while (it.hasNext()) {
+                writeNodeEdges(graph, it, bos);
+            }
+        }
     }
 }
