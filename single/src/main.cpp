7145394715e83e80277340d2e85bd900a5142985
Robin Voetter <robin@voetter.nl>
2020-11-04 16:19:48 +0100

    Makefile + input reading for single

---
+++ single/src/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <filesystem>
+#include <fstream>
+#include <cstdlib>
+#include <cstdint>
+#include <algorithm>
+
+using NodeId = uint32_t;
+
+class Graph {
+    private:
+        NodeId max_node_id;
+        std::vector<NodeId> sources;
+        std::vector<NodeId> dests;
+
+    public:
+        inline size_t nodes() const {
+            return this->max_node_id + 1;
+        }
+
+        inline size_t edges() const {
+            return this->sources.size();
+        }
+
+        template <typename T>
+        void map_edges(T callback) const {
+            for(size_t i = 0; i < sources.size(); ++i) {
+                callback(sources[i], dests[i]);
+            }
+        }
+
+        void add_edge(NodeId a, NodeId b) {
+            this->sources.push_back(a);
+            this->dests.push_back(b);
+            this->max_node_id = std::max({a, b, this->max_node_id});
+        }
+};
+
+Graph read_graph(const char* path) {
+    auto g = Graph();
+
+    for (auto& entry : std::filesystem::directory_iterator(path)) {
+        auto f = std::ifstream(entry.path());
+        if (!f) {
+            std::cerr << "Failed to open " << entry.path() << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        uint8_t x[8];
+        while (f.read(reinterpret_cast<char*>(x), sizeof(x))) {
+            NodeId src = x[0] | (x[1] << 8) | (x[2] << 16) | (x[3] << 24);
+            NodeId dst = x[4] | (x[5] << 8) | (x[6] << 16) | (x[7] << 24);
+            g.add_edge(src, dst);
+        }
+    }
+
+    return g;
+}
+
+void pagerank(const Graph& graph, size_t iterations, float alpha) {
+    std::vector<float> a;
+    a.resize(graph.nodes(), 0.0f);
+    std::vector<float> b;
+    b.resize(graph.nodes(), 0.0f);
+    std::vector<NodeId> d;
+    d.resize(graph.nodes(), 0);
+
+    graph.map_edges([&](NodeId x, NodeId y) {
+        d[x] += 1;
+    });
+
+    for(size_t iter = 0; iter < iterations; ++iter) {
+        for(size_t i = 0; i < graph.nodes(); ++i) {
+            b[i] = alpha * a[i] / d[i];
+            a[i] = 1.0f - alpha;
+        }
+
+        graph.map_edges([&](NodeId x, NodeId y) {
+            a[y] += b[x];
+        });
+    }
+}
+
+int main(int argc, const char* argv[]) {
+    if (argc <= 1) {
+        std::cout << "Usage: " << argv[0] << " <edges directory>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    auto g = read_graph(argv[1]);
+
+    std::cout << g.edges() << " " << g.nodes() << std::endl;
+
+    return EXIT_SUCCESS;
+}
