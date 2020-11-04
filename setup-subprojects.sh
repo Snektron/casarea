e0bf149e62970bb5cdfda877d2ce048f339926d8
Robin Voetter <robin@voetter.nl>
2020-11-04 19:52:06 +0100

    Also compile single automatically

--- setup-subprojects.sh
+++ setup-subprojects.sh
@@ -2,6 +2,8 @@
 # This script compiles subprojects such as the webgraph extractor, and spark/c++ implementations
 # It should be called from casarea.sh
 
+set -eu
+
 # Compile webgraph-extract
 "$CASAREA_DATADIR/software/gradle/bin/gradle" \
     -b "$CASAREA_ROOT/webgraph-extract/build.gradle" \
@@ -13,3 +15,6 @@
     -b "$CASAREA_ROOT/spark/build.gradle" \
     --quiet \
     install
+
+# Compile the single-core implementation
+make -C "$CASAREA_ROOT/single/" --quiet
