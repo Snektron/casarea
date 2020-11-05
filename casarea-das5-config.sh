9469d242e526d97b11b6d194057174de2495888a
DDPS course <ddps2017@fs0.cm.cluster>
2020-11-05 22:06:19 +0100

    single core results for smaller graphs

--- casarea-das5-config.sh
+++ casarea-das5-config.sh
@@ -7,19 +7,19 @@ export CASAREA_WORKDIR="/var/scratch/ddps2017/casarea/work"
 
 # The graphs which the tests should be performed on. (useful to test with smaller
 # graphs). These graphs are downloaded from http://data.law.it.unimi.it/.
-export CASAREA_TEST_GRAPHS="uk-2014-host uk-2014-tpd"
+export CASAREA_TEST_GRAPHS="uk-2014-tpd uk-2014-host"
 
 # Whether compute jobs should be run locally or through prun
 export CASAREA_RUN_LOCAL=0
 
 # Number of times tests should be ran
-export CASAREA_REPETITIONS=1
+export CASAREA_REPETITIONS=5
 
 # Number of cores the spark tests should be ran on
-export CASAREA_CORE_TESTS="32" 
+export CASAREA_CORE_TESTS="128" 
 
 # The timeout for every spark job
-export CASAREA_PRUN_TIMEOUT="00:15:00"
+export CASAREA_PRUN_TIMEOUT="00:30:00"
 
 # Not used by casarea.sh but still useful to put here
 export JAVA_HOME="$CASAREA_DATADIR/software/openjdk"
