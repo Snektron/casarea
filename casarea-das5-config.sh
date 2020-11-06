5f8c2755ee6a0af1c3748ef08c356acb683ad6ca
DDPS course <ddps2017@fs0.cm.cluster>
2020-11-06 12:35:57 +0100

    Das5 config: set to user's scratch directory by default

--- casarea-das5-config.sh
+++ casarea-das5-config.sh
@@ -1,13 +1,13 @@
 # The root of the directory where static data (software, datasets, etc) should be stored
-export CASAREA_DATADIR="/var/scratch/ddps2017/casarea/data"
+export CASAREA_DATADIR="/var/scratch/$USER/casarea/data"
 
 # The root of the directory where temporary information should be stored during
 # the program runtime
-export CASAREA_WORKDIR="/var/scratch/ddps2017/casarea/work"
+export CASAREA_WORKDIR="/var/scratch/$USER/casarea/work"
 
 # The graphs which the tests should be performed on. (useful to test with smaller
 # graphs). These graphs are downloaded from http://data.law.it.unimi.it/.
-export CASAREA_TEST_GRAPHS="cnr-2000"
+export CASAREA_TEST_GRAPHS="uk-2014-host uk-2014-tpd"
 
 # Whether compute jobs should be run locally or through prun
 export CASAREA_RUN_LOCAL=0
@@ -16,10 +16,10 @@ export CASAREA_RUN_LOCAL=0
 export CASAREA_REPETITIONS=5
 
 # Number of cores the spark tests should be ran on
-export CASAREA_CORE_TESTS="8 16 32" 
+export CASAREA_CORE_TESTS="8 16 32 64 128" 
 
 # The timeout for every spark job
-export CASAREA_PRUN_TIMEOUT="00:30:00"
+export CASAREA_PRUN_TIMEOUT="00:15:00"
 
 # Not used by casarea.sh but still useful to put here
 export JAVA_HOME="$CASAREA_DATADIR/software/openjdk"
